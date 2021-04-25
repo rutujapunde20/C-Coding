5a1ab714445ef63bc0a7651093189f9f7659833a
rutujapunde20 <83077427+rutujapunde20@users.noreply.github.com>
2021-04-25 12:00:32 +0530

    Create increment.c

---
+++ increment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int ch,j=0,k=40;
+	
+	//increment
+	ch=80;
+	ch++;
+	
+	printf("\n=========Output=========\n");
+	printf("\n ch=%d ch=%d ch=%d ch=%d \n",ch,ch++,ch--,ch);
+	
+}
